2da83d75d9c45adb643361c9aaa04352b7361fae
Michael Mendenhall <michael.mendenhall@nist.gov>
2015-08-11 15:33:08 -0400

    EField analytical calculations updates

--- Scripts/EField/EndField.py
+++ Scripts/EField/EndField.py
@@ -8,35 +8,97 @@ from pyx import graph
 from pyx.color import rgb, hsb
 from pyx.graph.style import symbol
 
+# pre-compute bessel function zeros
+bessel_j0n = [0,]+list(special.jn_zeros(0, 1000))
+
 class BesselCalcs:
-	def __init__(self):
-		self.j0n = [0,]+list(special.jn_zeros(0, 1000))
-		self.cns = [0,] + [self.cn(n+1) for n in range(100)]
-	def cn(self,n):
-		return 2/(self.j0n[n]*special.jv(1,self.j0n[n]))
-	def V(self, r, z):
-		s = 0
-		for (n,c) in enumerate(self.cns):
-			if n==0:
-				continue
-			s += self.cns[n]*exp(-self.j0n[n]*z)*special.jv(0,self.j0n[n]*r)
-		return s
+    """Field for flat mismatched-voltage endcap on cylinder"""
+    def __init__(self, r0 = 1):
+        # radius scale units
+        self.r0 = r0
+        # pre-compute coefficients
+        self.ncoeffs = 101
+        self.cns = [0 for n in range(self.ncoeffs)]
+        
+    def add_endcircle(self, r, V):
+        """Add circular patch of given radius and voltage"""
+        for n in range(self.ncoeffs)[1:]:
+            self.cns[n] += V*self.cn(n, r)
+        
+    def cn(self,n,ri):
+        """Expansion coefficient"""
+        if self.r0 == ri:
+            return 2/(bessel_j0n[n]*special.jv(1,bessel_j0n[n]))
+        r = ri/self.r0
+        return 2*special.jv(1,r*bessel_j0n[n])/(bessel_j0n[n]*special.jv(1,bessel_j0n[n])**2)
+    
+    def V(self, r, z):
+        """Potential as a function of r,z position"""
+        r /= self.r0
+        z /= self.r0
+        s = 0
+        for (n,c) in enumerate(self.cns):
+            if n==0:
+                continue
+            s += self.cns[n]*exp(-bessel_j0n[n]*z)*special.jv(0,bessel_j0n[n]*r)
+        return s
 
+    def Er(self, r, z):
+        """Radial field component dV/dr"""
+        r /= self.r0
+        z /= self.r0
+        s = 0
+        for (n,c) in enumerate(self.cns):
+            if n==0:
+                continue
+            s += self.cns[n]*exp(-bessel_j0n[n]*z)*bessel_j0n[n]*special.jvp(0,bessel_j0n[n]*r)
+        return s/self.r0
+    
+    def Ez(self, r, z):
+        """axial field component dV/dz"""
+        r /= self.r0
+        z /= self.r0
+        s = 0
+        for (n,c) in enumerate(self.cns):
+            if n==0:
+                continue
+            s += -bessel_j0n[n]*self.cns[n]*exp(-bessel_j0n[n]*z)*special.jv(0,bessel_j0n[n]*r)
+        return s/self.r0 
+
+class TwoEndedBessel:
+    def __init__(self,dz,BC1,BC2=None):
+        if not BC2:
+            BC2 = BesselCalcs(BC1.r0)   # grounded end
+        for n in range(len(BC1.cns))[1:]:
+            ez = exp(-bessel_j0n[n]*dz/BC1.r0)
+            k = 1./(1-ez**2)
+            BC1.cns[n], BC2.cns[n] = (BC1.cns[n]-ez*BC2.cns[n])*k, (-ez*BC1.cns[n]+BC2.cns[n])*k
+        self.BC1 = BC1
+        self.BC2 = BC2
+        self.dz = dz
+        
+    def V(self, r, z):
+        return self.BC1.V(r,z) + self.BC2.V(r, self.dz-z)
+    def Er(self, r, z):
+        return self.BC1.Er(r,z) + self.BC2.Er(r, self.dz-z)
+    def Ez(self, r, z):
+        return self.BC1.Ez(r,z) - self.BC2.Ez(r, self.dz-z)
+    
 if __name__ == "__main__":
-	BC = BesselCalcs()
-	print BC.j0n
-	for n in range(200)[1:]:
-		print "\t%i\t& %g\t\\\\"%(n, BC.cn(n))
+    BC = BesselCalcs()
+    print BC.j0n
+    for n in range(200)[1:]:
+        print "\t%i\t& %g\t\\\\"%(n, BC.cn(n))
 
-	g = graph.graphxy(width=10,height=6,
-        	x=graph.axis.lin(title="radial position $r/r_0$"),
-        	y=graph.axis.lin(title="endcap contribution $V(r,z)/V_1$"))
-	
-	for z in [0.1*n for n in range(25)]:
-		npts = 50
-		if z==0:
-			npts = 600
-		gdat = [ (r, BC.V(r,z)) for r in numpy.linspace(0,1,400) ]
-		g.plot(graph.data.points(gdat,x=1,y=2),[graph.style.line(lineattrs=[style.linewidth.thin,rgb.blue])])
-    	g.writetofile("endfield.pdf")
+    g = graph.graphxy(width=10,height=6,
+            x=graph.axis.lin(title="radial position $r/r_0$"),
+            y=graph.axis.lin(title="endcap contribution $V(r,z)/V_1$"))
+    
+    for z in [0.1*n for n in range(25)]:
+        npts = 50
+        if z==0:
+            npts = 600
+        gdat = [ (r, BC.V(r,z)) for r in numpy.linspace(0,1,400) ]
+        g.plot(graph.data.points(gdat,x=1,y=2),[graph.style.line(lineattrs=[style.linewidth.thin,rgb.blue])])
+        g.writetofile("endfield.pdf")
 
--- Scripts/EField/elemesholve_Scan.py
+++ Scripts/EField/elemesholve_Scan.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
+import os
 from math import *
+from EndField import *
+
 from pyx import *
 from pyx import style
 from pyx import graph
@@ -17,24 +20,95 @@ def rainbowDict(keys, b=1.0):
     return dict([ (k,hsb((1.0*x)/n,1,b)) for (x,k) in enumerate(knew) ])
         
 class GridPt:
+    """Grid scan point in field map"""
     def __init__(self, fline):
         d = [float(x) for x in fline.split()]
         self.x = d[:3]
         self.phi = d[3]
         self.E = d[4:]
+        self.Er = 0
+        r = self.r()
+        if r:
+            self.Er = (self.E[0]*self.x[0] + self.E[1]*self.x[1])/r
+            
     def dist(self, gp):
+        """Distance from another point"""
         return sqrt(sum([(self.x[i]-gp.x[i])**2 for i in range(3)]))
-        
-def load_scans(fname):
+    def r(self):
+        """Radial coordinate"""
+        return sqrt(self.x[0]**2 + self.x[1]**2)
+    def add(self, pt):
+        """add another point's field"""
+        self.phi += pt.phi
+        self.Er += pt.Er
+        for i in range(3):
+            self.E[i] += pt.E[i]
+    def mul(self, c):
+        """multiply field by a constant"""
+        self.phi *= c
+        self.Er *= c
+        for i in range(3):
+            self.E[i] *= c
+            
+class ScanLine:
+    def __init__(self, pts):
+        self.pts = pts
+        self.npts = len(self.pts)
+        # coordinate mean and standard deviation
+        self.mu = [sum([p.x[i] for p in self.pts])/self.npts for i in range(3)]
+        self.rms = [sqrt(sum([(p.x[i]-self.mu[i])**2 for p in self.pts])/self.npts) for i in range(3)]
+        # identify varying axis
+        for i in range(3):
+            if self.rms[i] > 0.01:
+                self.varaxis = i
+    def r(self):
+        return sqrt(self.mu[0]**2 + self.mu[1]**2)
+            
+def average_lines(slines):
+    lens = [s.npts for s in slines]
+    lens.sort()
+    medlen = lens[len(lens)/2]
+    print len(slines), medlen
     
+    nsum = 0
+    ss = None
+    for s in slines:
+        if s.npts != medlen:
+            continue
+        nsum += 1
+        if not ss:
+            ss = s
+            continue
+        for (n,p) in enumerate(s.pts):
+            ss.pts[n].add(p)
+    for p in ss.pts:
+        p.mul(1./nsum)
+    
+    return ss
+    
+def load_scans(fname):
+    """Load scan data, separated into lines"""
+    
+    # load separate scan lines
     scans = [ ]
     for p in [GridPt(l) for l in open(fname).readlines()]:
         if scans and p.dist(scans[-1][-1]) < 0.2:
             scans[-1].append(p)
         else:
             scans.append([p,])
-            
-    return scans
+    print "Found", len(scans), "scans."
+    
+    # group by radius
+    scangroups = [ ]
+    for s in scans:
+        l = ScanLine(s)
+        if scangroups and abs(l.r() - scangroups[-1][-1].r()) < 0.01:
+            scangroups[-1].append(l)
+        else:
+            scangroups.append([l,])
+    
+    print "\tin", len(scangroups), "radius groups."
+    return [average_lines(g) for g in scangroups]
 
 
     
@@ -43,32 +117,53 @@ def plot_scans(fname):
     cols = rainbow(len(scans))
     
     gPhi = graph.graphxy(width=10,height=6,
-        x=graph.axis.lin(title="position [cm]"),
-        y=graph.axis.log(title="potential [kV]"))
+        x=graph.axis.lin(title="position [cm]", min = -0.5, max = 3),
+        y=graph.axis.lin(title="potential [kV]", max = 0.04))
     
-    gEt = graph.graphxy(width=10,height=6,
-        x=graph.axis.lin(title="position [cm]"),
-        y=graph.axis.lin(title="transverse field [kV/cm]", min = -0.01, max = 0.01))
+    gEt = graph.graphxy(width=12,height=8,
+        x=graph.axis.lin(title="position [cm]", min = -8, max = 8),
+        y=graph.axis.lin(title="transverse field [V/cm]", min = -4, max = 0.5),
+        key=graph.key.key(pos="bl"))
     
     gEa = graph.graphxy(width=10,height=6,
         x=graph.axis.lin(title="position [cm]", min = -1, max = 1),
         y=graph.axis.lin(title="axial field [kV/cm]"))
     
+    V = 4.1
+    BC1 = BesselCalcs(5.5)
+    BC1.add_endcircle(4.0, V)
+    
+    BC2 = BesselCalcs(10.)
+    BC2.add_endcircle(10, 1.7*V)
+    BC2.add_endcircle(5.5, -1.7*V)
+    BC2.add_endcircle(4.0, V)
+    BC2 = TwoEndedBessel(10, BC2)
+    
+    def bccombo(r,z):
+        if z < 0:
+            return BC1.Er(r,-z)
+        return BC2.Er(r,z)
+    
     for (n,sc) in enumerate(scans):
-        gdat = [(p.x[2], p.phi, sqrt(p.E[0]**2 + p.E[1]**2), p.E[0], p.E[1], p.E[2]) for p in sc]
+        print sc.r(), sc.npts
+        gdat = [(p.x[2], p.phi, 1000*p.Er, p.E[0], p.E[1], p.E[2]) for p in sc.pts]
+        gtitle = "$r = %.1f$ cm"%sc.r()
         
-        plotl = [graph.style.line([cols[n]])]
-        plotl2 = [graph.style.line([cols[n], style.linestyle.dotted])]
+        plotl = [graph.style.line([cols[n], style.linewidth.thin])]
+        plotl2 = [graph.style.line([cols[n], style.linestyle.dashed, style.linewidth.THin])]
         
         gPhi.plot(graph.data.points(gdat,x=1,y=2), plotl)
-        gEt.plot(graph.data.points(gdat,x=1,y=4), plotl)
+        gEt.plot(graph.data.points(gdat,x=1,y=3,title=gtitle), plotl)
         #gEt.plot(graph.data.points(gdat,x=1,y=5), plotl2)
         gEa.plot(graph.data.points(gdat,x=1,y=6), plotl)
+        
+        gdat_endsim = [(p.x[2], bccombo(p.r(),p.x[2])) for p in sc.pts[::5]]
+        gEt.plot(graph.data.points(gdat_endsim,x=1,y=2,title=None), plotl2)
         
     gPhi.writetofile("FieldScans.pdf")
     gEt.writetofile("FieldScans_Et.pdf")
     gEa.writetofile("FieldScans_Ea.pdf")
     
 if __name__ == "__main__":
-    plot_scans("~/Applications/elemesholve-bld/scan.txt")
+    plot_scans(os.environ["APP_DIR"]+"/elemesholve-bld/scan.txt")
     
